8cc4dbfac7963f0d98054b2425f36aa56d35273f
roli <bryanrolandos@gmail.com>
2015-04-05 01:53:47 -0600

    Create readin.h

---
+++ src/readin.h
@@ -0,0 +1,10 @@
+#ifndef READIN_H
+#define READIN_H
+
+char *gets_unl(int tsknte);
+char *readstdin(void);
+char *delmem(char *str);
+int want_date(void);
+
+size_t is_null(char *ptr);
+#endif // -> READIN_H
