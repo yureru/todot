9975f1afa6c6b4da7e25d06f28d99a40d4a5727c
roli <bryanrolandos@gmail.com>
2015-04-09 01:02:58 -0500

    Update readin.h
    proto and <stdio.h>

--- src/readin.h
+++ src/readin.h
@@ -1,10 +1,11 @@
 #ifndef READIN_H
 #define READIN_H
-
+#include <stdio.h>
 char *gets_unl(int tsknte);
 char *readstdin(void);
 char *delmem(char *str);
 int want_date(void);
 
 size_t is_null(char *ptr);
+char *gets_unl_f(FILE *db);
 #endif // -> READIN_H
