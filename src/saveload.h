48b88e7ef7ef08a3ef3c1afe20dfcc79016b424b
roli <bryanrolandos@gmail.com>
2015-04-09 01:05:39 -0500

    Update saveload.h
    proto

--- src/saveload.h
+++ src/saveload.h
@@ -1,6 +1,8 @@
 #ifndef SAVELOAD_H
 #define SAVELOAD_H
+#include <stdio.h>
 #include "list.h"
+
 /* File text has the form:
 [task]text of the task
 [task]yes this is another task
@@ -12,4 +14,5 @@
 int load_data(struct node *list, struct data *sd);
 int save_data(struct data *sd, FILE *db);
 int flush_line(FILE *db);
+void new_item(int taskornote, int *wastask, struct data *sd, char *str);
 #endif // SAVELOAD_H
